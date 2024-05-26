a6153f89ac642c460a0006eb99445ad815ee8ded
b1g_ph4rm4 <132758165+Gvte-Kali@users.noreply.github.com>
2024-05-26 15:15:45 +0200

    Test ASCII Art

--- BadUSB/RubberDucky_PoC/RubberDucky_PoC.ps1
+++ BadUSB/RubberDucky_PoC/RubberDucky_PoC.ps1
@@ -6,21 +6,42 @@ OpenNotepad
 }
 
 function OpenNotepad {
-    # Ouvrir le Bloc-notes
+    # Open Notepad
     Start-Process notepad
     Start-Sleep -Seconds 2
     
-    # Maximiser la fenêtre du Bloc-notes
-    MaximizeNotepad
-    
-    # Augmenter la police d'écriture
-    SetNotepadFontSize -fontSize 24
-    
-    # Écrire le texte spécifié
+    # Writes the specified text
     $wshell = New-Object -ComObject wscript.shell
     $wshell.AppActivate('Notepad')
     Start-Sleep -Seconds 1
     $wshell.SendKeys("Bonjour,`r`n`r`nVoici une demonstration de ce qu'une cle USB infectee peut faire :")
+    $wshell.SendKeys("`r⠀⠀⠀⠀⠀⣠⣴⣶⣿⣿⠿⣷⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣷⠿⣿⣿⣶⣦⣀⠀⠀⠀⠀⠀`r")
+    $wshell.SendKeys("⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣶⣦⣬⡉⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠚⢉⣥⣴⣾⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⡾⠿⠛⠛⠛⠛⠿⢿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⠿⠿⠛⠛⠛⠛⠿⢧⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⣠⣤⠶⠶⠶⠰⠦⣤⣀⠀⠙⣷⠀⠀⠀⠀⠀⠀⠀⢠⡿⠋⢀⣀⣤⢴⠆⠲⠶⠶⣤⣄⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠘⣆⠀⠀⢠⣾⣫⣶⣾⣿⣿⣿⣿⣷⣯⣿⣦⠈⠃⡇⠀⠀⠀⠀⢸⠘⢁⣶⣿⣵⣾⣿⣿⣿⣿⣷⣦⣝⣷⡄⠀⠀⡰⠂⠀`r)
+    $wshell.SendKeys("⠀⠀⣨⣷⣶⣿⣧⣛⣛⠿⠿⣿⢿⣿⣿⣛⣿⡿⠀⠀⡇⠀⠀⠀⠀⢸⠀⠈⢿⣟⣛⠿⢿⡿⢿⢿⢿⣛⣫⣼⡿⣶⣾⣅⡀⠀`r)
+    $wshell.SendKeys("⢀⡼⠋⠁⠀⠀⠈⠉⠛⠛⠻⠟⠸⠛⠋⠉⠁⠀⠀⢸⡇⠀⠀⠄⠀⢸⡄⠀⠀⠈⠉⠙⠛⠃⠻⠛⠛⠛⠉⠁⠀⠀⠈⠙⢧⡀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡇⢠⠀⠀⠀⢸⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⡇⠀⠀⠀⠀⢸⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠟⠁⣿⠇⠀⠀⠀⠀⢸⡇⠙⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠰⣄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⠖⡾⠁⠀⠀⣿⠀⠀⠀⠀⠀⠘⣿⠀⠀⠙⡇⢸⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠄⠀`r)
+    $wshell.SendKeys("⠀⠀⢻⣷⡦⣤⣤⣤⡴⠶⠿⠛⠉⠁⠀⢳⠀⢠⡀⢿⣀⠀⠀⠀⠀⣠⡟⢀⣀⢠⠇⠀⠈⠙⠛⠷⠶⢦⣤⣤⣤⢴⣾⡏⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠈⣿⣧⠙⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⢊⣙⠛⠒⠒⢛⣋⡚⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⡿⠁⣾⡿⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠘⣿⣇⠈⢿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⡿⢿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⡟⠁⣼⡿⠁⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠘⣿⣦⠀⠻⣿⣷⣦⣤⣤⣶⣶⣶⣿⣿⣿⣿⠏⠀⠀⠻⣿⣿⣿⣿⣶⣶⣶⣦⣤⣴⣿⣿⠏⢀⣼⡿⠁⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠘⢿⣷⣄⠙⠻⠿⠿⠿⠿⠿⢿⣿⣿⣿⣁⣀⣀⣀⣀⣙⣿⣿⣿⠿⠿⠿⠿⠿⠿⠟⠁⣠⣿⡿⠁⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠈⠻⣯⠙⢦⣀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⣠⠴⢋⣾⠟⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⡀⠈⠉⠒⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠐⠒⠉⠁⢀⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⡀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢀⡴⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
+    $wshell.SendKeys("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`r)
     Start-Sleep -Seconds 5
 
     # Fermer le Bloc-notes directement via le processus
@@ -28,77 +49,6 @@ function OpenNotepad {
     if ($notepad) {
         $notepad.Kill()
     }
-}
-
-function MaximizeNotepad {
-    # Utiliser les API Windows pour maximiser la fenêtre du Bloc-notes
-    Add-Type @"
-using System;
-using System.Runtime.InteropServices;
-public class User32 {
-    [DllImport("user32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool SetForegroundWindow(IntPtr hWnd);
-}
-"@
-
-    $maxAttempts = 20
-    $attempt = 0
-    $hwnd = [IntPtr]::Zero
-
-    while ($hwnd -eq [IntPtr]::Zero -and $attempt -lt $maxAttempts) {
-        $hwnd = [User32]::FindWindow("Notepad", $null)
-        if ($hwnd -eq [IntPtr]::Zero) {
-            $hwnd = [User32]::FindWindowEx([IntPtr]::Zero, [IntPtr]::Zero, $null, "Sans titre - Bloc-notes")
-        }
-        if ($hwnd -eq [IntPtr]::Zero) {
-            $hwnd = [User32]::FindWindowEx([IntPtr]::Zero, [IntPtr]::Zero, $null, "Untitled - Notepad")
-        }
-        Start-Sleep -Milliseconds 500
-        $attempt++
-    }
-
-    if ($hwnd -ne [IntPtr]::Zero) {
-        [User32]::SetForegroundWindow($hwnd)
-        # SW_MAXIMIZE = 3
-        [User32]::ShowWindow($hwnd, 3)
-    } else {
-        Write-Host "Notepad window not found."
-    }
-}
-
-function SetNotepadFontSize {
-    param (
-        [int]$fontSize = 24
-    )
-
-    # Définir la taille de la police dans le registre
-    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Notepad" -Name "lfHeight" -Value (-$fontSize * 10)
-
-    # Redémarrer Notepad pour appliquer les modifications
-    $notepad = Get-Process notepad -ErrorAction SilentlyContinue
-    if ($notepad) {
-        $notepad.Kill()
-    }
-    
-    Start-Sleep -Seconds 1
-
-    # Réouvrir Notepad
-    Start-Process notepad
-    Start-Sleep -Seconds 2
-    
-    # Rémaximiser après le redémarrage
-    MaximizeNotepad
 }
 
 
